0de26073974853f724ffd81fafd86dd42bdff092
876733271@qq.com <lizheng9293@gmail.com>
2020-06-30 22:50:57 +0800

    线上调试

--- src/libs/api.request.js
+++ src/libs/api.request.js
@@ -2,7 +2,7 @@ import HttpRequest from './axios'
 let config  = {
     baseUrl: {
         dev: '/api',
-        pro: 'https://music.2hakeji.com'
+        pro: 'localhost:3000'
     }
 }
 const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
